bef8a6bdf662e4d51fb5d4a66f1c6a69d74a6a72
Bas de Bruin <bassie00001@outlook.com>
2026-07-17 16:59:04 +0200

    Initial CSV grade updater

---
+++ update_grades.py
@@ -0,0 +1,25 @@
+import csv
+
+BONUS = 5
+
+with open("students.csv", "r", newline="") as infile:
+    reader = csv.DictReader(infile)
+
+    updated_students = []
+
+    for row in reader:
+        grade = int(row["Grade"])
+        grade = min(100, grade + BONUS)
+
+        updated_students.append({
+            "Name": row["Name"],
+            "Grade": grade
+        })
+
+with open("updated_students.csv", "w", newline="") as outfile:
+    writer = csv.DictWriter(outfile, fieldnames=["Name", "Grade"])
+
+    writer.writeheader()
+    writer.writerows(updated_students)
+
+print("Finished! Updated grades saved to updated_students.csv")
